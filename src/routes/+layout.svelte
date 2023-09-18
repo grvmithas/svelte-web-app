242e2088ae664f011565b8ace1c27bf03bef7d67
Gaurav Mithas <gaurav.mithas@publicissapient.com>
2023-09-18 14:55:29 +0530

    svelte route setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import {page, navigating} from "$app/stores";
+</script>
+
+<nav>
+  <a href="/" aria-current={$page.url.pathname === "/"}>Home</a>
+  <a href="/about" aria-current={$page.url.pathname === "/about"}>about</a>
+  <a href="/pschat" aria-current={$page.url.pathname === "/pschat"}>PSchat</a>
+</nav>
+
+<slot />
---
+++ src/routes/pschat/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import {page, navigating} from "$app/stores";
+</script>
+
+<h1>This is PSChat page</h1>
