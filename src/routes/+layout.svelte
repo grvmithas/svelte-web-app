2cae05b78f82ace1399c197d04487129921faa83
ashishmangla0 <ashishmangla1991@gmail.com>
2023-09-19 14:12:41 +0530

    add new code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,19 @@
 
 <Skipnav />
 <Nav />
+
 <main id="main">
   <slot />
 </main>
+
+
+<style>
+main{
+  width: calc(100% - 230px);
+  height: 100vh;
+  overflow: auto;
+  background: #eaedf7;
+  padding: 0 1.5rem;
+}
+
+</style>
