ff410c2c4be0b968e2b0c38153138d6d4621ee7a
ashishmangla <ashishmangla1991@gmail.com>
2023-10-09 08:35:31 +0530

    add code for mule initializr

--- src/routes/muleinitializr/+page.svelte
+++ src/routes/muleinitializr/+page.svelte
@@ -1,23 +1,20 @@
 <script>
-  import {page, navigating} from "$app/stores";
+  import { page, navigating } from "$app/stores";
 </script>
+
 <section>
   <h1 class="main-content-title">mule initializr</h1>
 
-  <input type="text" class=""/>
+  <input type="text" class="" />
 
   <div class="input-group mb-3">
-    <input class="form-control input-group-form" placeholder="Search Exchange" type="text">
+    <input
+      class="form-control input-group-form"
+      placeholder="Search Exchange"
+      type="text"
+    />
     <button class="input-group-text">
-      <i></i>
+      <i />
     </button>
-<span class="input-group-text" id="basic-addon2">@example.com</span>
   </div>
-
 </section>
-
-
-
-<h1>This is test data</h1>
-
-
